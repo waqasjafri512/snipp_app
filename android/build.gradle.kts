d2f56a99ab11751829349df3b4a36a0bf1e94682
waqasjafri512 <mr.waqasjafri@gmail.com>
2026-04-27 23:22:39 +0500

    feat: configure Android build settings and integrate Firebase Messaging and Local Notifications dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,17 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+    
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
 }
 
 tasks.register<Delete>("clean") {
